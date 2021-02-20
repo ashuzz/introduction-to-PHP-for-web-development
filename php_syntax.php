65f890a8463ad41e37652f670ba18a8068390036
Ashenafi Kebede <ashuewa@gmail.com>
2021-02-20 04:07:02 +0100

    Update php_syntax.php

--- php_syntax.php
+++ php_syntax.php
@@ -1,8 +1,8 @@
 //This is a practice file.
 <?php 
-//You can decalre php variables by appending the dollar sign $ at the beginning of the variable.
+//You can declare php variables by appending the dollar sign $ at the beginning of the variable.
 
-/* PHP Variables
+/* PHP Variables name convention
 A variable starts with the $ sign, followed by the name of the variable.
 A variable name must start with a letter or the underscore character.
 A variable name cannot start with a number.
