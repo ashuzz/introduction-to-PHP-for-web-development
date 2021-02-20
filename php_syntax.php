b01395c4c2e1709bfb58f73e5276a3055cfad752
Ashenafi Kebede <ashuewa@gmail.com>
2021-02-20 03:42:49 +0100

    Working with variables

--- php_syntax.php
+++ php_syntax.php
@@ -1,6 +1,14 @@
-
+//This is a practice file.
 <?php 
 //This a basic print out code
-
+/*
 echo "Practice to master it !";
+*/
+
+//You can decalre php variables by appending the dollar sign $ at the beginning of the variable.
+
+$message = "Practice again again";
+
+echo $message;
+
 ?>
