191044fb2cd70b3dda6c1ce93d748f51b69cca45
Ashenafi Kebede <ashuewa@gmail.com>
2021-02-20 04:01:50 +0100

    Update php_syntax.php

--- php_syntax.php
+++ php_syntax.php
@@ -1,11 +1,13 @@
 //This is a practice file.
 <?php 
-//This a basic print out code
-/*
-echo "Practice to master it !";
-*/
-
 //You can decalre php variables by appending the dollar sign $ at the beginning of the variable.
+
+/* PHP Variables
+A variable starts with the $ sign, followed by the name of the variable.
+A variable name must start with a letter or the underscore character.
+A variable name cannot start with a number.
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+*/
 
 $message = "Practice again again";
 
